1e143c993ad24e23780842cf3887d6749f0abda3
efimpolianskii <17petz@gmail.com>
2022-12-20 16:09:39 +0400

    commit rain

---
+++ rain/js/all.js
@@ -0,0 +1,301 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="keywords" content="Телеканал Дождь, официальный сайт. Круглосуточное онлайн вещание. Программа передач. Информация о программах канала и их ведущих. Архив видеоматериалов.">
+    <meta name="description" content="Телеканал Дождь, Наталья Синдеева, Вера Кричевская, Поэт и гражданин, Конституция, Здесь и Сейчас, Михаил Зыгарь, Жизнь, Валерий Панюшкин">
+    <link rel="canonical" href="https://tvrain.tv/login/">
+    <meta property="og:title" content="Телеканал Дождь, Авторизация">
+    <meta property="og:description" content="Телеканал для тех, кому не все равно">
+    <meta property="og:image" content="../images/tvrain-logo-fb.png">
+    <meta property="og:type" content="tv_show">
+    <meta property="fb:app_id" content="188964134476198">
+    <meta property="og:url" content="https://tvrain.tv/login/">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@tvrain">
+    <meta name="twitter:creator" content="@tvrain">
+    <link title="Все материалы" rel="alternate" type="application/rss+xml" href="https://tvrain.tv/export/rss/all.xml">
+    <title>Телеканал ДОЖДЬ - Ошибка</title>
+    <meta name="apple-itunes-app" content="app-id=955281863">
+    <meta name="apple-mobile-web-app-title" content="Телеканал Дождь">
+    <meta name="application-name" content="Телеканал Дождь">
+    <meta name="msapplication-TileColor" content="#cf3476">
+    <meta name="msapplication-TileImage" content="/static/favicons/mstile-144x144.png">
+    <meta name="msapplication-config" content="/static/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#9b1e55">
+    <meta property="fb:pages" content="324852143799">
+    <link href="//fonts.googleapis.com/css?family=Roboto:400,500,300&amp;subset=latin,cyrillic" rel="stylesheet" type="text/css">
+    <link rel="apple-touch-icon" sizes="57x57" href="../images/apple-touch-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="../images/apple-touch-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="../images/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="../images/apple-touch-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="../images/apple-touch-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="../images/apple-touch-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="../images/apple-touch-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="../images/apple-touch-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="../images/apple-touch-icon-180x180.png">
+    <link rel="icon" type="image/png" href="../images/favicon-32x32.png" sizes="32x32">
+    <link rel="icon" type="image/png" href="../images/android-chrome-192x192.png" sizes="192x192">
+    <link rel="icon" type="image/png" href="../images/favicon-96x96.png" sizes="96x96">
+    <link rel="icon" type="image/png" href="../images/favicon-16x16.png" sizes="16x16">
+    <link rel="shortcut icon" href="../favicon.ico">
+    <link rel="mask-icon" href="../fonts/safari-pinned-tab.svg" color="#cf3476">
+    <link rel="manifest" href="/manifest.json">
+    <link rel="stylesheet" type="text/css" href="../css/app_1.css">
+    <script src="adfox.min.js"></script>
+    <script src="jquery.min.js"></script>
+    <script src="jquery.cookie.js"></script>
+    <script src="jquery.modal.min.js" async></script>
+    <script src="lodash.min.js"></script>
+    <script src="blazy.min.js"></script>
+    <script>
+        timestamp = 1467732407;
+        TVRAIN = {};
+        TVRAIN.UserSubscription = {
+            info: {
+                subscriber: "0",
+                old: "0",
+                new: "0",
+                promo5: "0",
+                promo10: "0",
+                promo10smarttv: "0",
+                promo30ref: "0",
+                event: "0",
+                noAds: "0",
+                alacarte: "0",
+                live: "0",
+                last_was_promo: "0",
+                subscription_outdate_period: "0",
+                subscription_outdate_product: [],
+                days_left: "0",
+                dimension: "guest"
+            }
+        };
+        TVRAIN.promo_enabled = "0";
+    </script>
+    <script src="all.js"></script>
+    <script type="text/javascript">
+        initGA(null);
+    </script>
+    <script>
+        if (new AdaptiveHelper().getScreenBreakpointName() === 'mobile' && localStorage && localStorage.getItem('force_wide_version') != 1) {
+            document.write('<meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">');
+        } else {
+            document.write('<meta name="viewport" content="width=1280">');
+        }
+    </script>
+    <script>
+        ! function (f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function () {
+                n.callMethod ? n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '962900387111723');
+        fbq('track', "PageView");
+    </script>
+    <!-- time: 2016-07-05 18:26:47 -->
+</head>
+
+<body>
+    <script type="text/javascript" src="tvrainanalytics.js"></script>
+    <script type="text/javascript">
+        /*<![CDATA[*/
+        TvrainAnalytics.init({
+            "module": "new",
+            "controller": "site",
+            "action": "login",
+            "id": null,
+            "url": "https:\/\/tvrain.tv\/login\/",
+            "referer": "https:\/\/tvrain.tv\/",
+            "user_id": 0,
+            "is_subscriber": 0
+        }); /*]]>*/
+    </script>
+
+    <div class="wrap wrap--transparent">
+        <div class="layout layout--white">
+            <div class="menu">
+                <a href="/" class="menu__logo">
+                    <div class="logo logo--menu"></div>
+                </a>
+                <div class="menu__line menu__line--live"> <a href="/live/" class="menu__live">Прямой эфир</a>
+                </div>
+                <div class="menu__stock">
+                    <div class="stocks_nest stocks_nest--in_menu">
+                        <div class="stocks stocks--in_menu">
+                            <div class="stocks__item stocks__item--in_menu" title="USD">
+                                <div class="stocks__item__title stocks__item__title--in_menu"> <span class="stocks__item__title__icon stocks__item__title__icon--in_menu" data-svg="/static/svg/dollar.svg"></span> </div>
+                                <div class="stocks__item__value stocks__item__value--in_menu stocks__item__value--up">
+                                    65,17
+                                    <div class="stocks__diff stocks__diff--in_menu stocks__diff--up" data-svg="/static/svg/caret.svg"></div>
+                                </div>
+                            </div>
+                            <div class="stocks__item stocks__item--in_menu" title="EUR">
+                                <div class="stocks__item__title stocks__item__title--in_menu"> <span class="stocks__item__title__icon stocks__item__title__icon--in_menu" data-svg="/static/svg/euro.svg"></span> </div>
+                                <div class="stocks__item__value stocks__item__value--in_menu stocks__item__value--down">
+                                    72,02
+                                    <div class="stocks__diff stocks__diff--in_menu stocks__diff--down" data-svg="/static/svg/caret.svg"></div>
+                                </div>
+                            </div>
+                            <div class="stocks__item stocks__item--in_menu" title="BRENT">
+                                <div class="stocks__item__title stocks__item__title--in_menu"> <span class="stocks__item__title__icon stocks__item__title__icon--in_menu" data-svg="/static/svg/tint.svg"></span> </div>
+                                <div class="stocks__item__value stocks__item__value--in_menu stocks__item__value--up">
+                                    49,41
+                                    <div class="stocks__diff stocks__diff--in_menu stocks__diff--up" data-svg="/static/svg/caret.svg"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+<!--                <div class="menu__podpiska"> <a href="/podpiska/"-->
+<!--                        class="menu__podpiska__button menu__podpiska__button&#45;&#45;promo menu__podpiska__button&#45;&#45;promo&#45;&#45;paypal">-->
+<!--                        Подписка<br> <span>С PayPal на месяц дольше</span> </a> </div>-->
+                <div id="adblock_message_nest"></div>
+                <div class="menu__line menu__line--nav">
+                    <div class="menu__items">
+                        <div class="menu__items__wrap"> <a href="/news/" class="menu__items__item">Новости</a>
+                            <a href="/teleshow/" class="menu__items__item">Программы</a>
+                            <a href="/people/" class="menu__items__item">Лица</a>
+                            <div class="menu__items__item menu__items__item--more">
+                                <div class="menu__items__more"> <a href="/shop/" class="menu__items__item">Магазин</a>
+                                    <a href="/archive/" class="menu__items__item">Архив</a>
+                                    <a href="/schedule/" class="menu__items__item">Расписание</a> <a href="/connecting/" class="menu__items__item">Как подключиться</a> <a href="/helpcenter/" class="menu__items__item">Центр поддержки</a> <a href="/notes/" class="menu__items__item">Заметки</a>
+                                    <script>
+                                        if (new AdaptiveHelper().getScreenBreakpointName() == 'mobile') {
+                                            document.write('<div class="menu__items__item"><div class="button button--inline button--med button--brand  __from_tablet_hidden __pc_hidden" onclick="MobileVersionHelper().toggleMode();">Полная версия сайта</div></div>');
+                                            document.write('<div class="menu__items__item"><div class="button button--inline button--med button--brand __until_tablet_hidden __pc_hidden" onclick="MobileVersionHelper().toggleMode();">Мобильная версия сайта</div></div>');
+                                        }
+                                    </script>
+                                </div>
+                                <span class="menu__items__item__more_text">Еще</span>
+                                <span class="menu__items__item__dropdown_ico">
+                                    <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="18px" height="9.938px" viewBox="0 0 18 9.938" enable-background="new 0 0 18 9.938" xml:space="preserve"><path d="M17.691,0.304c-0.41-0.406-1.076-0.405-1.486,0L9,7.435L1.794,0.304C1.385-0.1,0.716-0.1,0.306,0.304 c-0.408,0.403-0.408,1.062,0,1.466l7.949,7.864c0.411,0.404,1.077,0.404,1.488,0l7.947-7.864 C18.102,1.365,18.102,0.708,17.691,0.304z"/></svg>
+                                </span>
+                            </div>
+                        </div>
+                        <div class="menu__items__burger">
+                            <div class="menu__items__burger__line"></div>
+                            <div class="menu__items__burger__line"></div>
+                            <div class="menu__items__burger__line"></div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <script>
+                window.menu = new Menu($('.menu__line--nav'), $('.menu__items__wrap'), $('.menu__items__more'), $('.menu__items__item--more'), $('.menu__search'), $('.menu__profile'));
+            </script>
+            <div class="single_col">
+                <div class="error_page">
+                    <h1>Ошибка 404</h1>
+                    <p>Запрошенный материал не найден.</p>
+                    <p>Если вы считаете, что произошла ошибка, сообщите, пожалуйста, на почту <a href="/cdn-cgi/l/email-protection#dcabb9be9ca8aaaebdb5b2f2a8aa"><span class="__cf_email__" data-cfemail="d6a1b3b496a2a0a4b7bfb8f8a2a0">[email&#160;protected]</span></a>.</p>
+                </div>
+            </div>
+        </div>
+        <div class="footer">
+            <a href="/" class="logo logo--footer" data-svg="/static/svg/tvrain-slashes.svg"></a>
+            <div class="footer_wrap">
+                <div class="footer_nav_col">
+                    <div class="footer_nav_col__head">Смотрите</div>
+                    <a href="/teleshow/" class="footer-nav__item">Программы</a>
+                    <a href="/schedule/" class="footer-nav__item">Расписание</a>
+                    <a href="/live/" class="footer-nav__item">Эфир</a>
+                    <a href="/archive/" class="footer-nav__item">Архив</a>
+                </div>
+                <div class="footer_nav_col">
+                    <div class="footer_nav_col__head">Соцсети</div>
+                    <a href="https://youtube.com/tvrain" class="footer-nav__item">YouTube</a>
+                    <a href="https://twitter.com/tvrain" class="footer-nav__item">Twitter</a>
+                    <a href="https://telegram.im/tvrain" class="footer-nav__item">Telegram</a>
+                    <a href="https://www.facebook.com/tvrain" class="footer-nav__item">Facebook</a>
+                    <a href="https://www.instagram.com/tvrain/" class="footer-nav__item">Instagram</a>
+                    <a href="https://www.tiktok.com/@tvrain_fans" class="footer-nav__item">TikTok</a>
+                </div>
+                <div class="footer_nav_col">
+                    <div class="footer_nav_col__head">О нас</div>
+                    <a href="/vacancy/" class="footer-nav__item">Вакансии</a>
+                    <a href="/advertisement/" class="footer-nav__item">Реклама</a>
+                    <a href="/contacts/" class="footer-nav__item">Контакты</a>
+                </div>
+                <div class="footer_nav_col">
+                    <div class="footer_nav_col__head">Пользователям</div>
+<!--                    <a href="/oferta/" class="footer-nav__item">Соглашение</a>-->
+                    <a href="/helpcenter/" class="footer-nav__item">Служба поддержки</a>
+                    <a href="/connecting/" class="footer-nav__item">Как подключиться</a>
+                </div>
+                <div class="footer_nav_col">
+                    <div class="footer_nav_col__head">Смартфоны</div>
+                    <a href="https://itunes.apple.com/ru/app/dozd/id955281863" class="footer-nav__item">iOS</a>
+                    <a href="https://play.google.com/store/apps/details?id=com.hintsolutions.raintv" class="footer-nav__item">Android</a>
+                    <a href="https://www.microsoft.com/en-us/store/apps/ТВ-Дождь/9wzdncrcwpqb" class="footer-nav__item">Windows Phone</a>
+                </div>
+                <div class="footer-copy">
+                    &copy;&nbsp;Все права защищены. Любое использование материалов допускается только
+                    с&nbsp;согласия редакции.
+                    <br>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div id="fb-root"></div>
+    <script data-cfasync="false" src="email-decode.min.js"></script><script type="text/javascript" src="activeform.js"></script>
+    <script type="text/javascript">
+        /*<![CDATA[*/
+        jQuery(function ($) {
+            jQuery('#login-form').yiiactiveform({
+                'validateOnSubmit': true,
+                'validateOnChange': true,
+                'attributes': [{
+                    'id': 'User_password',
+                    'inputID': 'User_password',
+                    'errorID': 'User_password_em_',
+                    'model': 'User',
+                    'name': 'password',
+                    'enableAjaxValidation': true,
+                    'clientValidation': function (value, messages, attribute) {
+                        if (jQuery.trim(value) == '') {
+                            messages.push("\u041d\u0435\u043e\u0431\u0445\u043e\u0434\u0438\u043c\u043e \u0437\u0430\u043f\u043e\u043b\u043d\u0438\u0442\u044c \u043f\u043e\u043b\u0435 \u00ab\u041f\u0430\u0440\u043e\u043b\u044c\u00bb.");
+                        }
+                    }
+                }, {
+                    'id': 'User_email',
+                    'inputID': 'User_email',
+                    'errorID': 'User_email_em_',
+                    'model': 'User',
+                    'name': 'email',
+                    'enableAjaxValidation': true,
+                    'clientValidation': function (value, messages, attribute) {
+                        if (jQuery.trim(value) == '') {
+                            messages.push("\u041d\u0435\u043e\u0431\u0445\u043e\u0434\u0438\u043c\u043e \u0437\u0430\u043f\u043e\u043b\u043d\u0438\u0442\u044c \u043f\u043e\u043b\u0435 \u00ab\u042d\u043b\u0435\u043a\u0442\u0440\u043e\u043d\u043d\u0430\u044f \u043f\u043e\u0447\u0442\u0430\u00bb.");
+                        }
+                        if (jQuery.trim(value) != '') {
+                            if (value.length < 1) {
+                                messages.push("\u042d\u043b\u0435\u043a\u0442\u0440\u043e\u043d\u043d\u0430\u044f \u043f\u043e\u0447\u0442\u0430 \u0441\u043b\u0438\u0448\u043a\u043e\u043c \u043a\u043e\u0440\u043e\u0442\u043a\u0438\u0439 (\u041c\u0438\u043d\u0438\u043c\u0443\u043c: 1 \u0441\u0438\u043c\u0432.).");
+                            }
+                            if (value.length > 128) {
+                                messages.push("\u042d\u043b\u0435\u043a\u0442\u0440\u043e\u043d\u043d\u0430\u044f \u043f\u043e\u0447\u0442\u0430 \u0441\u043b\u0438\u0448\u043a\u043e\u043c \u0434\u043b\u0438\u043d\u043d\u044b\u0439 (\u041c\u0430\u043a\u0441\u0438\u043c\u0443\u043c: 128 \u0441\u0438\u043c\u0432.).");
+                            }
+                        }
+                    }
+                }],
+                'errorCss': 'error'
+            });
+        }); /*]]>*/
+    </script>
+</body>
+
+</html>
